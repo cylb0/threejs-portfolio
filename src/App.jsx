cb29ffee9e662537831d3e8e4ca4db375b626a33
Morgan <morgan.foucaut@greta-formations.fr>
2023-10-26 15:46:54 +0200

    Added dependencies and empty components

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter } from "react-router-dom"
+
+import { Navbar, Hero, About, Experience, Tech, Works, Contact} from './components'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div className="relative z-0">
+          <Contact />
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
